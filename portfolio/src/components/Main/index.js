267f6597470b594c95ec4a95144f13fb1e50b2a9
Shannon Ervin <sjervin07@gmail.com>
2020-04-06 18:32:48 -0500

    created browser router

--- portfolio/src/components/Main/index.js
+++ portfolio/src/components/Main/index.js
@@ -1,10 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Landing from '../Landing';
+import About from '../About';
+import Projects from '../Projects';
+import Resume from '../Resume';
+import Contact from '../Contact'
+import {Switch, Route} from 'react-router-dom';
 
-class Main extends Component {
-  render() {
-    return (
-      <div><h1>Landing Page</h1></div>
-    )
-  }
-}
+const Main = () => (
+    <Switch>
+        <Route exact path= "/" component={Landing}/>
+        <Route path = "/aboutme" component={About}/>
+        <Route path= "/portfolio" component={Projects}/>
+        <Route path= "/resume" component={Resume}/>
+        <Route path= "/contact" component={Contact}/>
+    </Switch>
+)
+
 export default Main;
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -1,17 +1,3 @@
-import React from "react";
-
-class Header extends React.Component {
-  render() {
-    return(
-<div className="jumbotron text-center p-3 mb-2 bg-secondary text-white" >
-  <h1 className="display-4 text-center "><strong>Web Developer Portfolio</strong></h1>
-  </div>
-
-    );
-  }
-}
-
-export default Header
 import React, {Component} from 'react';
 
 class Resume extends Component {
