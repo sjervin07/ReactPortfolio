232b293a9d87f0c0cfad6b7b7ee9fae843139ba1
Shannon Ervin <sjervin07@gmail.com>
2020-04-06 17:24:15 -0500

    Changed components

---
+++ portfolio/src/components/Main/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div><h1>Landing Page</h1></div>
+    )
+  }
+}
+export default Main;
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -12,3 +12,13 @@ class Header extends React.Component {
 }
 
 export default Header
+import React, {Component} from 'react';
+
+class Resume extends Component {
+  render() {
+    return (
+      <div><h1>Resume</h1></div>
+    )
+  }
+}
+export default Resume;
