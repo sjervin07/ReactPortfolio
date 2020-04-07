2d0945f5a708cb2ce36095a2887727bb8f06e730
Shannon Ervin <sjervin07@gmail.com>
2020-04-06 22:04:18 -0500

    styling of landing page, links updated

--- portfolio/src/components/Landing/index.js
+++ portfolio/src/components/Landing/index.js
@@ -1,10 +1,43 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class Landing extends Component {
   render() {
     return (
-      <div><h1>Landing Page</h1></div>
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <div className="banner-text">
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <h1>Full Stack Web Developer</h1>
+            <hr/>
+
+          <p>HTML/CSS | Bootstrap | JavaScript | NodeJS | Express | React | MySQL | MongoDB</p>
+
+        <div className="social-links">
+
+          {/* LinkedIn */}
+          <a href="www.linkedin.com/in/shannon-ervin-86a637196" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-linkedin-square" aria-hidden="true" />
+          </a>
+
+          {/* Github */}
+          <a href="https://github.com/sjervin07" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+          </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
+
 export default Landing;
